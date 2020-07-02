a0efecdcc8f6897948e2d536884526099f935697
sumayya-github <66043790+sumayya-github@users.noreply.github.com>
2020-07-02 15:24:02 +0530

    kmean first commit

--- kmean-model.py
+++ kmean-model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-dataset=pd.read_csv("/root/weblog.csv")
+dataset=pd.read_csv("/root/httpdlog.csv")
 dataset.head()
 dataset=dataset[['IP','Status']]
 dataset.head()
