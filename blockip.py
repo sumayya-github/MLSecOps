dcf3f5753287298239a49dabf24232cd80af857e
sumayya-github <66043790+sumayya-github@users.noreply.github.com>
2020-07-03 12:36:49 +0530

    block first commit

--- blockip.py
+++ blockip.py
@@ -1,5 +1,5 @@
 import os
-with open("/root/ml+ops-task5/blocked.txt","r") as file1:
+with open("/root/mlops_task5/blocked.txt","r") as file1:
 	for i in file1:
 		os.system('iptables -A INPUT -s {} -j DROP'.format(i.rstrip('\n')))
 	os.system('service iptables save')
