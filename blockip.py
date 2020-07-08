3004469ff6fd11a8cc3a806e754814029dfb0e0f
sumayya-github <66043790+sumayya-github@users.noreply.github.com>
2020-07-08 22:25:42 +0530

    Update blockip.py

--- blockip.py
+++ blockip.py
@@ -1,5 +1,5 @@
 import os
-with open("/workspace/blocked.txt","r") as file1:
+with open("/mlt3/blocked.txt","r") as file1:
 	for i in file1:
 		os.system('iptables -A INPUT -s {} -j DROP'.format(i.rstrip('\n')))
 	os.system('service iptables save')
